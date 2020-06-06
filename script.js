3a14d6282193571e33e9b66350208d9ed53dde5b
roberto.urbani <thetenebrax23@gmail.com>
2020-06-06 16:03:28 +0200

    Test con animazione reverse

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('anim'),
     renderer: 'svg',
-    loop: true,
-    autoplay: true,
+    loop: false,
+    autoplay: false,
     path: 'data.json'
 })
+
+var div = document.getElementById('anim');
+
+div.onmouseenter = function () {
+        animation.setDirection(1);
+        animation.goToAndPlay(0)
+    // animation.play();
+}
+
+div.onmouseleave = function () {
+    animation.setDirection(-1);
+
+
+    // ferma completamente l'animazione e resetta i frame
+    // animation.stop();
+}
